ab5882bce9a4c7add851a37c4258a5f376ce0660
villatrue <scarter0313@gmail.com>
2019-06-18 16:31:10 -0400

    properties/service  views incomplete

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -8,36 +8,36 @@ class ServicesController < ApplicationController
     @service = Service.find(params[:id])
   end
 
-  # def new
-  #   @service = Service.new
-  # end
-  #
-  # def create
-  #   @service = self.new(service_params)
-  #   @service.save
-  #   redirect_to service_path(@service.id)
-  # end
-  #
-  # def edit
-  #   @service = Services.find(params[:id])
-  # end
-  #
-  #
-  # def update
-  #   @service = Services.find(params[:id])
-  #   @service.update(service_params)
-  #   redirect_to service_path(@service.id)
-  # end
-  #
-  # def destroy
-  #   @service = Services.find(params[:id])
-  #   @service.destroy
-  #   redirect_to service_path
-  # end
-  #
-  # private
-  #
-  # def service_params
-  #   params.require(:service).permit(:!UPDATE VARIABLES!)
-  # end
+  def new
+    @service = Service.new
+  end
+  
+  def create
+    @service = self.new(service_params)
+    @service.save
+    redirect_to service_path(@service.id)
+  end
+  
+  def edit
+    @service = Services.find(params[:id])
+  end
+  
+  
+  def update
+    @service = Services.find(params[:id])
+    @service.update(service_params)
+    redirect_to service_path(@service.id)
+  end
+  
+  def destroy
+    @service = Services.find(params[:id])
+    @service.destroy
+    redirect_to service_path
+  end
+  
+  private
+  
+  def service_params
+    params.require(:service).permit(:property_id, :employee_id, :appointment, :service_cost, :materials_cost)
+  end
 end
