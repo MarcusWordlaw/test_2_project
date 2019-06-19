8ab2a37d1554445ac9b2a3b3bef5dd5b3e32227d
villatrue <scarter0313@gmail.com>
2019-06-18 21:38:03 -0400

    finished views added links

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,41 +8,6 @@ class EmployeesController < ApplicationController
     @employee = Employee.find(params[:id])
   end
 
-<<<<<<< HEAD
-#   def new
-#     @employee = Employee.new
-#   end
-  
-#   def create
-#     @employee = self.new(employee_params)
-#     @employee.save
-#     redirect_to employee_path(@employee.id)
-#   end
-  
-#   def edit
-#     @employee = Employee.find(params[:id])
-#   end
-  
-  
-#   def update
-#     @employee = Employee.find(params[:id])
-#     @employee.update(employee_params)
-#     redirect_to employee_path(@employee.id)
-#   end
-  
-#   def destroy
-#     @employee = Employee.find(params[:id])
-#     @employee.destroy
-#     redirect_to employees_path
-#   end
-
-#   private
-  
-#   def employee_params
-#     params.require(:employee).permit(:!UPDATE VARIABLES!)
-#   end
-# end
-=======
   def new
     @employee = Employee.new
   end
@@ -76,4 +41,3 @@ class EmployeesController < ApplicationController
     params.require(:employee).permit(:name, :title)
   end
 end
->>>>>>> second_branch
--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -13,31 +13,31 @@ class PropertiesController < ApplicationController
   end
   
   def create
-    @property = self.new(property_params)
+    @property = Property.new(property_params)
     @property.save
     redirect_to property_path(@property.id)
   end
-  #
-  # def edit
-  #   @property = Property.find(params[:id])
-  # end
-  #
-  #
-  # def update
-  #   @property = Property.find(params[:id])
-  #   @property.update(property_params)
-  #   redirect_to property_path(@property.id)
-  # end
-  #
-  # def destroy
-  #   @property = Property.find(params[:id])
-  #   @property.destroy
-  #   redirect_to properties_path
-  # end
-  #
-  # private
-  #
-  # def property_params
-  #   params.require(:property).permit(:!UPDATE VARIABLES!)
-  # end
+  
+  def edit
+    @property = Property.find(params[:id])
+  end
+  
+  
+  def update
+    @property = Property.find(params[:id])
+    @property.update(property_params)
+    redirect_to property_path(@property.id)
+  end
+  
+  def destroy
+    @property = Property.find(params[:id])
+    @property.destroy
+    redirect_to properties_path
+  end
+  
+  private
+  
+  def property_params
+    params.require(:property).permit(:name, :address, :zip_code, :pool_size, :pool_material, :pool_cover, :client_id)
+  end
 end
