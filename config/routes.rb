013d4f818c934c6f01a7f2b10a53fccc3aa8f772
github username <github email address>
2019-06-20 12:03:40 -0400

    routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,20 @@ Rails.application.routes.draw do
     resources:comments
   end
 
-  get '/login', to: 'sessions#new', as: 'login'
-  post '/login', to: 'sessions#create'
+
 
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy', as: 'logout'
   root 'welcomes#index'
+
   resources :properties
-  resources :services
   resources :employees
-  resources :client
+  resources :clients
+
+
+
+
 
 
 end
